4d03846f434461b8aca1ad92789a094a1c389711
Adam Podraza <apodra86@gmail.com>
2015-10-21 10:52:32 -0600

    paid and free flavors working

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -1,16 +1,15 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.builditbigger.paid;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.JokeServer;
 import com.example.adampodraza.myapplication.backend.myApi.MyApi;
@@ -21,6 +20,7 @@ import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.podraza.android.jokedisplay.JokeActivity;
+import com.udacity.gradle.builditbigger.R;
 
 import java.io.IOException;
 
@@ -38,14 +38,14 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        //AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
+        /**AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
-        mAdView.loadAd(adRequest);
+        mAdView.loadAd(adRequest);**/
 
         final String joke = JokeServer.getJoke();
 
